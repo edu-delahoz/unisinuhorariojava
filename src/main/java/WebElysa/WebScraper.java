ee626a4fa014b271da7af05b657afc0f5caa2df0
edu-delahoz <eduardrdodelahoz@gmail.com>
2023-11-15 09:50:53 -0500

    Version login

---
+++ src/main/java/WebElysa/WebScraper.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package WebElysa;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+
+
+/**
+ *
+ * @author eduar
+ */
+public class WebScraper {
+    
+}
